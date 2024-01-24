673931b78061ea526f3acc212095d8caea5ae42e
bbbowles <joaobowles@gmail.com>
2024-01-24 10:06:38 +0000

    adicionado tabela "images" para possibilitar o envio de diversas imagens e "linkalas" a um mesmo carro

--- src/database/knex/migrations/20230908110750_createCars.js
+++ src/database/knex/migrations/20230908110750_createCars.js
@@ -1,10 +1,9 @@
 exports.up = knex =>
-    knex.schema.createTable(`cars`,table =>{
+    knex.schema.createTable(`cars`, table => {
         table.increments("id")
-        table.text("name")
+        table.text("names")
         table.text("brand")
         table.integer("year")
         table.integer("user_id").references("id").inTable("users").onDelete("CASCADE")
-
     })
 exports.down = knex => knex.schema.dropTable('cars')
